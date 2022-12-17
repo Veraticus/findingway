d4ec325d89b50ae1d93e6ddbc84158d62e10e650
Yuyuri Yuri <yuyuriyuriravana@gmail.com>
2022-12-17 22:36:48 +0800

    Use a proper logger instead of just fmt to stdout
    Signed-off-by: Yuyuri Yuri <yuyuriyuriravana@gmail.com>

--- main.go
+++ main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"log"
 	"os"
 	"sync"
 	"time"
@@ -13,10 +12,18 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+var Logger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+
 func main() {
+
+	os.Exit(run())
+}
+
+func run() int {
 	discordToken, ok := os.LookupEnv("DISCORD_TOKEN")
 	if !ok {
-		panic("You must supply a DISCORD_TOKEN to start!")
+		Logger.Println("You must supply a DISCORD_TOKEN to start!")
+		return 1
 	}
 	once, ok := os.LookupEnv("ONCE")
 	if !ok {
@@ -27,26 +34,28 @@ func main() {
 		Token: discordToken,
 	}
 
-	config, err := ioutil.ReadFile("./config.yaml")
+	config, err := os.ReadFile("./config.yaml")
 	if err != nil {
-		panic(fmt.Errorf("Could not read config.yaml: %w", err))
+		Logger.Printf("Could not read config.yaml: %s\n", err)
+		return 1
 	}
 	yaml.Unmarshal(config, &d)
 
 	err = d.Start()
 	defer d.Session.Close()
 	if err != nil {
-		panic(fmt.Errorf("Could not instantiate Discord: %f", err))
+		log.Printf("Could not instantiate Discord: %s\n", err)
+		return 1
 	}
 
 	scraper := scraper.New("https://xivpf.com")
 
-	fmt.Printf("Starting Trappingway...\n")
+	Logger.Printf("Starting Trappingway...\n")
 	for {
-		fmt.Printf("Scraping source...\n")
+		Logger.Printf("Scraping source...\n")
 		err := scraper.Scrape()
 		if err != nil {
-			fmt.Printf("Scraper error: %f\n", err)
+			Logger.Printf("Scraper error: %f\n", err)
 			continue
 		}
 
@@ -54,26 +63,25 @@ func main() {
 		for _, channel := range d.Channels {
 			wg.Add(1)
 			go func(c *discord.Channel) {
-				fmt.Printf("Cleaning Discord for %v...\n", c.Duty)
+				Logger.Printf("Cleaning Discord for %v...\n", c.Duty)
 				err = d.CleanChannel(c.ID)
 				if err != nil {
-					fmt.Printf("Discord error cleaning channel: %f\n", err)
+					Logger.Printf("Discord error cleaning channel: %f\n", err)
 				}
 
-				fmt.Printf("Updating Discord for %v...\n", c.Duty)
+				Logger.Printf("Updating Discord for %v...\n", c.Duty)
 				err = d.PostListings(c.ID, scraper.Listings, c.Duty, c.DataCentres)
 				if err != nil {
-					fmt.Printf("Discord error updating messagea: %f\n", err)
+					Logger.Printf("Discord error updating messagea: %f\n", err)
 				}
 				wg.Done()
 			}(channel)
 		}
 		wg.Wait()
 		if once != "false" {
-			os.Exit(0)
+			return 0
 		}
 
 		time.Sleep(3 * time.Minute)
 	}
-
 }
